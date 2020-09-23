c678294e31271987ffcb2b3eed86c8bd6d773a2d
Preston Davis <prestonseandavis101@yahoo.com>
2020-09-23 22:08:46 +0000

    Automatically backed up by Learn

---
+++ lib/doctor.rb
@@ -0,0 +1,27 @@
+class Doctor
+  attr_accessor :name
+  @@all = []
+  
+  def initialize(name)
+    @name=name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select {|appointment| appointment.doctor == self}
+  end
+  
+  def patients
+   appointments.map do |appointment| appointment.patient
+  end
+end
+  
+  def new_appointment(patient, date)
+    Appointment.new(patient, date, self)
+  end
+  
+end
